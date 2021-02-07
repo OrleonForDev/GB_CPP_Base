fca0a9c832df3956f83919592f0dd2585a5cb0cd
OrleonForDev <gb.geekschool@gmail.com>
2021-02-07 20:16:02 +0400

    Complete 5 lesson

---
+++ Lesson_5/cmake-build-debug/CMakeFiles/Lesson_5.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lesson_5.dir/funcs.cpp.obj"
+  "CMakeFiles/Lesson_5.dir/main.cpp.obj"
+  "Lesson_5.exe"
+  "Lesson_5.exe.manifest"
+  "Lesson_5.lib"
+  "Lesson_5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lesson_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
