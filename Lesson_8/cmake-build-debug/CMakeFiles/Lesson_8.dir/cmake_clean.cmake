9c0f14a91137704122c7568e125919dd68f7b428
OrleonForDev <AkchurinIS@gmail.com>
2021-02-22 20:47:49 +0400

    Start Lesson_8

---
+++ Lesson_8/cmake-build-debug/CMakeFiles/Lesson_8.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lesson_8.dir/main.cpp.obj"
+  "Lesson_8.exe"
+  "Lesson_8.exe.manifest"
+  "Lesson_8.lib"
+  "Lesson_8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lesson_8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
